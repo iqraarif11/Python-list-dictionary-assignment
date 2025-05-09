254401d721d60d20f0ec8d5a26e2b861f5d139b8
Iqra Arif <bintearif79@gmail.com>
2025-05-09 06:39:40 -0700

    Add files via upload

---
+++ list ,dictionary practice.py
@@ -0,0 +1,24 @@
+#Part 1 :Lists (Example)
+fruits=["grapes","pomegranate","pineapple"]
+#Display all items in the list
+print("Fruits lists:",fruits)
+#Access a specific items in the list
+print("Second fruit:",fruits[1])
+#Add a new items in the  list
+fruits.append("strawberry")
+#Display the updated list
+print("updated fruits list:",fruits)
+#Part 2 :Dictionary (Example)
+student ={
+    "name": "Iqra Arif",
+    "age":  21,
+    "grade": "ADS"
+}
+#Display the entire dictionary
+print("Student Info:",student)
+#Access a specifice valu by key 
+print("Name:",student["name"])
+#Add a new key valu pair
+student["school"] = "Punjab Girls College"
+#Display the updated dictionary
+print("Updated Student Info:",student)
